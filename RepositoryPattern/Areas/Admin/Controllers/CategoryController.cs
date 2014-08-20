0fe411627fa1703cf011fbbd241badf82444216e
jarheghan <ejdprogrammer@gmail.com>
2014-08-20 16:35:40 -0500

    More View

---
+++ RepositoryPattern/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepositoryPattern.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        //
+        // GET: /Admin/Category/
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(int ss)
+        {
+            return View();
+        }
+
+        public ActionResult Edit(int categoryId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int categoryId)
+        {
+            return View();
+        }
+    }
+}
