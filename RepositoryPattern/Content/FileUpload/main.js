2219a0bd2c43615be53850d8e2f18d541d4ec358
jarheghan <ejdprogrammer@gmail.com>
2014-09-05 13:49:33 -0500

    all

--- RepositoryPattern/Content/FileUpload/main.js
+++ RepositoryPattern/Content/FileUpload/main.js
@@ -16,11 +16,46 @@ $(function () {
     'use strict';
 
     // Initialize the jQuery File Upload widget:
-    $('#fileupload').fileupload();
+   // $('#fileupload').fileupload();
 
-    $('#fileupload').fileupload('option', {
-            maxFileSize: 500000000,
-            resizeMaxWidth: 1920,
-            resizeMaxHeight: 1200
+    //$('#fileupload').fileupload('option', {
+    //        maxFileSize: 500000000,
+    //        resizeMaxWidth: 1920,
+    //        resizeMaxHeight: 1200
+    //});
+
+         var el = $('#fileupload'),
+        img = el.find('img'),
+        elHidden = el.find('.hidden'),
+        elRemove = el.find('.remove');
+
+        $('#fileupload1').fileupload({
+            url: '/Admin/Picture/AsyncUpload',
+            dataType: 'json',
+            //acceptFileTypes: /^image\/(gif|jpeg|jpg|png)$/,
+            acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
+
+            done: function (e, data) {
+                debugger;
+                var result = data.result;
+                if (result.success) {
+                    img.attr('src', result.imageUrl);
+                    elHidden.val(result.pictureId);
+                    elRemove.show();
+                }
+            },
+
+            error: function (jqXHR, textStatus, errorThrown) {
+                if (errorThrown === 'abort') {
+                    //alert('File Upload has been canceled');
+                }
+            }
+        });
+
+        elRemove.click(function (e) {
+            img.attr('src', '');
+            elHidden.val(0);
+            $(this).hide();
+            e.preventDefault();
         });
 });
