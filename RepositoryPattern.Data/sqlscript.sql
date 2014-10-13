ba76b4df66fc4f54bb45d4f75db55ad78af8121d
jarheghan <dscrollj@gmail.com>
2014-10-13 05:34:08 -0500

    All

--- RepositoryPattern.Data/sqlscript.sql
+++ RepositoryPattern.Data/sqlscript.sql
@@ -91,3 +91,47 @@ ALTER TABLE dbo.ProductVariation ADD CONSTRAINT
 GO
 COMMIT
 select Has_Perms_By_Name(N'dbo.ProductVariation', 'Object', 'ALTER') as ALT_Per, Has_Perms_By_Name(N'dbo.ProductVariation', 'Object', 'VIEW DEFINITION') as View_def_Per, Has_Perms_By_Name(N'dbo.ProductVariation', 'Object', 'CONTROL') as Contr_Per 
+
+
+
+USE [RPStore]
+GO
+
+/****** Object:  Table [dbo].[CustomerInfo]    Script Date: 10/13/2014 5:32:38 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[CustomerInfo](
+	[cus_id] [int] NOT NULL,
+	[cus_guid] [uniqueidentifier] NULL,
+	[cus_email] [nvarchar](1000) NULL,
+	[cus_first_name] [nvarchar](200) NULL,
+	[cus_last_name] [nvarchar](200) NULL,
+	[cus_address1] [nvarchar](300) NULL,
+	[cus_address2] [nvarchar](200) NULL,
+	[cus_address3] [nvarchar](200) NULL,
+	[cus_city] [nvarchar](100) NULL,
+	[cus_state] [nvarchar](100) NULL,
+	[cus_country] [nvarchar](100) NULL,
+	[cus_zip] [nvarchar](20) NULL,
+	[cus_phone] [nvarchar](20) NULL,
+	[cus_company] [nvarchar](100) NOT NULL,
+	[cus_admin_comment] [nvarchar](2000) NULL,
+	[cus_active] [bit] NULL,
+	[cus_add_user] [nvarchar](100) NULL,
+	[cus_add_date] [datetime] NULL,
+	[cus_change_user] [nvarchar](100) NULL,
+	[cus_change_date] [datetime] NULL,
+	[cus_delete_flag] [bit] NULL,
+ CONSTRAINT [Customer_PK_Customer] PRIMARY KEY CLUSTERED 
+(
+	[cus_id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
